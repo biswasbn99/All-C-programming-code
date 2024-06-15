e094e88a650697892f3571bf1b853778af8b668a
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-06-16 01:00:03 +0600

    Average of 3 float numbers

---
+++ Average of 3 float number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+    float a,b,c,Aver;
+    printf("Input value of a,b and c:\n");
+    scanf("%f%f%f",&a,&b,&c);
+    Aver=(a+b+c)/3;
+    printf("Average=%.2f",Aver);
+}
