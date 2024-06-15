64e8047eed09b7bc87697fc5d3f79e5d291a26f9
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-06-16 00:15:53 +0600

    Area & Perimeter of a Rectangle

---
+++ Area & Perimeter of A  Rectangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    int x,y,peri,area;
+    printf("Enter the value Of x & y:");
+    scanf("%d%d",&x,&y);
+    peri=2*(x+y);
+    area=x*y;
+    printf("perimeter of the rectangle:%d \n and area of the rectangle:%d",peri,area);
+        }
