4ee8f5b329a109e0279d664089a2edd193736c27
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-06-16 00:52:25 +0600

    A program larger between 2 number

---
+++ A program larger between 2 numbers.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+    int x,y,m;
+    printf("Enter the value of x and y:");
+    scanf("%d%d",&x,&y);
+    m=(x>y)?((printf)("x is larger")):((printf)("y is larger"));
+}
