5c09b952f46edc86dc517d83487b3e99e1ec1c3d
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-06-16 00:21:07 +0600

    Find the third angle of a triangle

---
+++ Third Angle of a Triangle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+    int x,y,z;
+        printf("The third angle of a triangle:");
+            scanf("%d%d",&x,&y);
+            z=(180-(x+y));
+            printf("The third angle of a triangle:%d",z);
+}
