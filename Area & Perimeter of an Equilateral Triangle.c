0c106d3c033869ce4c2c97863d2da68d4074cf71
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-06-16 00:18:45 +0600

    Area & Perimeter of an Equilateral Triangle

---
+++ Area & Perimeter of an Equilateral Triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    float x,area,peri;
+    printf("Enter the value of x:");
+    scanf("%f",&x);
+    area=(float)(0.433*x*x);
+    peri=(3*x);
+    printf("area of an equilateral triangle: \n and perimeter of an equilateral triangle:",area,peri);
+    }
