5f9d5e70b40c77aa455a472560aba2b9759db169
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-06-15 23:53:37 +0600

    print some string

---
+++ About Your Self.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+main()
+{
+    printf("My Name:Bn Biswas\nDate Of Birth:15-12-02\nMobile Number:+8801456-956754\nHome District:barisal\nSSC:Binodpur High School\nHSC:Mirpur High School & Collage\nMy Hobby:Reading Books\nTravelling,Hearing Songs etc.\nPresent Address:dhaka\nEmail Address:biswashridoy8@gmai.com");
+}
