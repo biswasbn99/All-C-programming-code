f46825ec8ba93f378dbb988a3e0c5bb98328cf36
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-06-16 10:05:05 +0600

    Area & Perimeter of a Square

---
+++ Area & Perimeter of a Square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    int l,A,P;
+    printf("Input the value of l:\n");
+    scanf("%d",&l);
+    A=(l*l);
+    P=(4*l);
+    printf("Area of a Square is:%d\n Perimeter of a Square is:%d",A,P);
+}
