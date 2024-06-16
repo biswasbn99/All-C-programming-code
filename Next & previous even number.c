4fdc9b03af6070ace84f26752114dd7d865c6362
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-06-16 10:11:23 +0600

    Next & previous odd number of an inputted value

---
+++ Next & previous even number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+    int c,x,y;
+    printf("Input the value of c:\n");
+    scanf("%d",&c);
+
+    x=(c+2);
+    y=(c-2);
+    printf("The next even number is:%d \n The previous even number is:%d",x,y);
+}
