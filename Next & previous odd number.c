392a981c6e34663fc6572b0229f39005412cea4f
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-06-16 10:09:56 +0600

    Next & previous odd number of an inputted value

---
+++ Next & previous odd number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+    int c,x,y;
+    printf("Input the value of c:\n");
+    scanf("%d",&c);
+
+    x=(c+2);
+    y=(c-2);
+    printf("Next odd number is:%d \n Previous odd number is:%d",x,y);
+}
