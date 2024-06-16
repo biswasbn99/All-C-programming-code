4cd231f69adc2a5804d783ab26f4a9e4fb158b23
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-06-16 10:08:04 +0600

    Find next & previous integer of an inputted value

---
+++ Find next & previous integer of an inputted value.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+    int c,x,y;
+    printf("Input the value of c:\n");
+    scanf("%d",&c);
+
+    x=(c+1);
+    y=(c-1);
+    printf("The next integer is:%d \n The previous integer is:%d",x,y);
+}
