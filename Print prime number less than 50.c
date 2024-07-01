5ba557d83ab3a22b98bebee1a968ad2e48227e88
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-07-01 22:10:34 +0600

    print all prime numbers less than 50

---
+++ Print prime number less than 50.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+    int n,d,f;
+    printf("All print is:\n");
+    for(n=1;n<=50;n++)
+    {
+        f=0;
+        for(d=2;d<=n/2;d++)
+    {
+        if(n%d==0)
+        {
+        f++;
+        break;
+
+        }
+
+
+    }
+    if(f==1)
+    {
+        printf("%d\t",n);
+    }
+
+    }
+
+}
+
