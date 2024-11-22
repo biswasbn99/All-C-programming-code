fcb2d695b87e9c3fbea0f7122dba803246c19563
BYADHANATH BISWAS <143856740+biswasbn99@users.noreply.github.com>
2024-11-23 00:25:07 +0600

    Dimond Patter(*)

---
+++ Dimond Pattern.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+main()
+{
+    int r,c;
+    for(r=1;r<=5;r++)
+    {
+        for(c=1;c<=5-r;c++)
+        {
+            printf("  ");
+        }
+        for(c=1;c<=2*r-1;c++)
+        {
+            printf("* ");
+
+        }
+        printf("\n");
+    }
+
+
+    for(r=4;r>=1;r--)
+    {
+        for(c=1;c<=5-r;c++)
+        {
+            printf("  ");
+        }
+        for(c=1;c<=2*r-1;c++)
+        {
+            printf("* ");
+
+        }
+        printf("\n");
+    }
+
+
+}
